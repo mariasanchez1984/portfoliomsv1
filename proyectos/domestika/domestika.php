d4970bd22627d7b4e3b08e4c6769263eaa846e95
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-04-04 00:54:54 +0200

    m

--- proyectos/domestika/domestika.php
+++ proyectos/domestika/domestika.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300&family=Ubuntu:ital,wght@1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
-    <link rel="stylesheet" href="../../../includes/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="./../../includes/css/style.css" type="text/css"/>
     <link rel="stylesheet" href="../../../includes/css/contacto.css" type="text/css"/>
     <link rel="stylesheet" href="../../../includes/fonts/style.css" type="text/css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
@@ -34,7 +34,7 @@
                 Página web: <a href="https://www.domestika.org/es/">https://www.domestika.org/es/</a>
     </div>
          
-    <?php include ('../../footer.php') ?>
+    <?php include ('./../../includes/footer.php') ?>
 
     <script src="./../../js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
