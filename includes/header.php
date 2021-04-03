904829882729c623c59ea57829fe3de64d391311
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-04-03 23:10:06 +0200

    m

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
 <header class="header">
         <nav class="main-nav">
             <ul class="menu" id="menu">
-                <li class="menu-item"><a href="../home.php" class="menu-link"><i class="fa fa-home"></i> Inicio</a></li>
+                <li class="menu-item"><a href="home.php" class="menu-link"><i class="fa fa-home"></i> Inicio</a></li>
                     <li class="menu-item container-submenu">
                         <a href="#" class="menu-link submenu-btn">
                             <i class="fas fa-user-graduate"></i> Formación 
@@ -25,9 +25,9 @@
                                 </li>
                             </ul>
                     </li>
-                <li class="menu-item"><a href="../sobre-mi.php" class="menu-link"><i class="far fa-address-card site-nav--icon"></i> Sobre mi</a></li>
-                <li class="menu-item"><a href="../contacto.php" class="menu-link"><i class="fas fa-user-edit site-nav--icon"></i> Contacto</a></li>
+                <li class="menu-item"><a href="sobre-mi.php" class="menu-link"><i class="far fa-address-card site-nav--icon"></i> Sobre mi</a></li>
+                <li class="menu-item"><a href="contacto.php" class="menu-link"><i class="fas fa-user-edit site-nav--icon"></i> Contacto</a></li>
             </ul>
         </nav>
-    <br><a href="../home.php" id="home"><i class="fa fa-home"></i> Home</a>
+    <br><a href="home.php" id="home"><i class="fa fa-home"></i> Home</a>
 </header>
