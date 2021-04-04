94256ba2ff5af3cb55dc64d89a56a4004455d84a
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-04-04 18:12:29 +0200

    m

--- proyectos/enterprise/proyectos-php/bingo/proyectobingo.php
+++ proyectos/enterprise/proyectos-php/bingo/proyectobingo.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300&family=Ubuntu:ital,wght@1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
-    <link rel="stylesheet" href="../../../../css/style.css" type="text/css"/>
-    <link rel="stylesheet" href="../../../../includes/css/contacto.css" type="text/css"/>
-    <link rel="stylesheet" href="../../../../includes/fonts/style.css" type="text/css"/>
+    <link rel="stylesheet" href="./../../../../includes/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="./../../../../includes/css/contacto.css" type="text/css"/>
+    <link rel="stylesheet" href="./../../../../includes/fonts/style.css" type="text/css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <title>Portfolio María S Vielba</title>
 </head>
 <body>
 
-<?php include ('./../../../../header.php') ?>
+<?php include ('./../../../../includes/header.php') ?>
     
     <div class= "contenido-formacion">
         <p class="titulo">- Generador de Cartones de Bingo -</p>
@@ -35,7 +35,7 @@
             </a>
     </div>
 
-    <?php include ('../../../..//footer.php') ?>
+    <?php include ('./../../../../includes/footer.php') ?>
     <script src="./../../../../js/menu.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
 </body>
