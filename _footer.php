65853030a7e5e4363f3e962cb765b7b884400bc8
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-04-07 21:04:28 +0200

    m

---
+++ _footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    &copy; All rights reserved. Diseño de: María S Vielba&nbsp&nbsp
+    <a href="contacto.php">~ Contacto ~</a>
+</div>
+<script src="./includes/js/menu.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+</body>
+</html>
