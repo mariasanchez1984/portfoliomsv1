b8741d3701adeaf9b51d7842efacf657d1eeb45e
María Sánchez Vielba <70698506+mariasanchez1984@users.noreply.github.com>
2021-04-08 23:25:50 +0200

    m

--- proyectos/enterprise/enterprise.php
+++ proyectos/enterprise/enterprise.php
@@ -13,10 +13,38 @@
     <title>Portfolio María S Vielba</title>
 </head>
 <body>
-
-    
-<?php include ('./../../includes/header.php') ?>
-
+<p class="logo"><i class="fas fa-code"></i> María S Vielba
+        <span class="subtitulo">~ Diseño/Maquetación Web ~</span>
+</p>
+    <span class="nav-bar" id="btnMenu">
+        <i class="fas fa-bars"></i>
+    </span>
+<header class="header">
+        <nav class="main-nav">
+            <ul class="menu" id="menu">
+                <li class="menu-item"><a href="home.php" class="menu-link"><i class="fa fa-home"></i> Inicio</a></li>
+                    <li class="menu-item container-submenu">
+                        <a href="#" class="menu-link submenu-btn">
+                            <i class="fas fa-user-graduate"></i> Formación 
+                            <i class="fa fa-chevron-down icono"></i>
+                        </a>
+                            <ul class="submenu">
+                                <li class="menu-item container-submenu">
+                                    <a href="proyectos/domestika/domestika.php" class="menu-link submenu-btn"> ~ Domestika</a>
+                                </li>
+                                <li class="menu-item container-submenu">
+                                    <a href="proyectos/enterprise/enterprise.php" class="menu-link submenu-btn"> ~ Enterprise Formación</a>
+                                </li>
+                                <li class="menu-item container-submenu">
+                                    <a href="proyectos/foro/foro.php" class="menu-link submenu-btn"> ~ Foro Training</a>
+                                </li>
+                            </ul>
+                    </li>
+                <li class="menu-item"><a href="sobre-mi.php" class="menu-link"><i class="far fa-address-card site-nav--icon"></i> Sobre mi</a></li>
+                <li class="menu-item"><a href="contacto.php" class="menu-link"><i class="fas fa-user-edit site-nav--icon"></i> Contacto</a></li>
+            </ul>
+        </nav>
+</header>
 <section class="contenido">
     <div class="columnas">
         <img class="im" src="certificado.png" alt="certificado">
@@ -81,9 +109,14 @@
                 <strong>Página web:</strong> <a href="http://enterpriseformacion.com/">http://enterpriseformacion.com/</a>
     </div>-->
     
-    <?php include ('./../../includes/footer.php') ?>
+    <div class="footer">
+        &copy; All rights reserved. Diseño de: María S Vielba&nbsp&nbsp
+            <a href="./../../contacto.php">~ Contacto ~</a>
+    </div>
 
-    <br><a href="./../../home.php" id="home"><i class="fa fa-home"></i> Home</a>
-    <script src="./../../includes/js/menu.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<a href="./../../home.php" id="home"><i class="fa fa-home"></i> Home</a>
+<script src="./../../includes/js/menu.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+</body>
+</html>
     
